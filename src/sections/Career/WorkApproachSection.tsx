2788a4f2e09f1f290757b9e72e0edc4f5f7f1b0c
ipf-yychoi <yychoi@iportfolio.co.kr>
2023-10-11 18:07:46 +0900

    Implement [IPFHOME-102] 우리가 일하는 방법 섹션 추가

---
+++ src/sections/Career/WorkApproachSection.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+import { responsive, high_resolution } from '../../layouts/responsive';
+
+import Button from '../../components/Button';
+import Container from '../../components/Container';
+import SubTitle from '../../components/SubTitle';
+import Description from '../../components/Description';
+
+import img_work_approach from '../../assets/images/Career/img_work_approach.png';
+import img_work_approach_2x from '../../assets/images/Career/img_work_approach@2x.png';
+
+const ContainerStyled = styled(Container)`
+  flex-wrap: nowrap;
+  gap: 4rem;
+
+  > div:first-child {
+    max-width: 32.2rem;
+    width: auto;
+
+    @media ${responsive.conditionForDesktop} {
+      max-width: 42.7rem;
+    }
+  }
+`;
+
+const WorkApproachImage = styled.span`
+  background-image: url(${img_work_approach});
+  @media ${high_resolution} {
+    background-image: url(${img_work_approach_2x});
+  }
+
+  width: 100%;
+  height: 28rem;
+  border-radius: 16px;
+
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  @media ${responsive.conditionForTablet} {
+    width: 33.6rem;
+    height: 28rem;
+  }
+
+  @media ${responsive.conditionForDesktop} {
+    width: 48rem;
+  }
+`;
+
+export default function WorkApproachSection() {
+  return (
+    <ContainerStyled
+      data-sal="slide-up"
+      data-sal-duration="1000"
+      data-sal-easing="ease"
+    >
+      <div>
+        <SubTitle>Introducing Our Unique Work Approach</SubTitle>
+        <Description>
+          {`With technology grounded in philosophy, we're on a mission to revolutionize English education.\nIn this journey, we share a mutual trust and continuously contemplate better directions together.`}
+        </Description>
+        <Button href="https://iportfolio.oopy.io/career" target="_blank">
+          View more
+        </Button>
+      </div>
+
+      <WorkApproachImage />
+    </ContainerStyled>
+  );
+}
